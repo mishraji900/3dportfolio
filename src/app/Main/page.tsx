1e45e65f2815f77520d139738d79ffabac05d20e
Mayank Mishra <117895206+mishraji900@users.noreply.github.com>
2024-03-05 15:41:50 +0530

    second commit

--- src/app/Main/page.tsx
+++ src/app/Main/page.tsx
@@ -3,9 +3,8 @@ import React from 'react'
 import Intro from "@/app/Intro/page"
 import About from "@/app/About/page"
 import Sec from "@/app/SEC/page"
-import {Contact} from "@/app/Contact/page"
+import Contact from "@/app/Contact/page"
 import Projects from '../Projects/page'
-import { BackgroundBoxesDemo } from './try'
 const page = () => {
   return (
     <div className='bg-[#000000]'>
